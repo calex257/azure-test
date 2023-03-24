396cfe33a3eeb161e3e51c6073dd7f36a6cb7963
Alexandru Calciu <alexandru.calciu@stud.acs.upb.ro>
2023-03-24 13:59:50 +0200

    Modify default route behaviour

--- app_src/routes/index.js
+++ app_src/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "the greatest website on planet earth" });
+    // res.render("index", { title: "the greatest website on planet earth" });
+    res.sendFile("index.html");
 });
 
 module.exports = router;
