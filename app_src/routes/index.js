80cb422e9dc92ac83449d4fef99d98fba2d583d2
Alexandru Calciu <alexandru.calciu@stud.acs.upb.ro>
2023-03-24 14:39:05 +0200

    Add process.cwd() to path

--- app_src/routes/index.js
+++ app_src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
     // res.render("index", { title: "the greatest website on planet earth" });
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(process.cwd() + "/public/index.html");
 });
 
 module.exports = router;
